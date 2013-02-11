379580153428fb10082e76f3cff5ea5af8170b07
posva <posva13@gmail.com>
2013-02-11 18:31:01 +0100

    Supressed old comments...

--- src/block.h
+++ src/block.h
@@ -46,38 +46,3 @@ inline position blockPosition(const block* b, uint16_t i) { assert(i < b->pos_si
 
 inline void blockSetPosition(const block* b, uint16_t i, position pos) { assert(i < b->pos_size); b->pos[i] = pos; }
 
-/*
-class Block {
-	std::vector<Position> m_pos;
-	BlockType m_type; //like color or something
-	
-	int16_t getLeftWidth() const;
-	
-public:
-	Block(uint16_t size = 4);
-	Block(const Block& copy);
-	~Block();
-	
-	
-	inline BlockType getType() const { return m_type; }
-	inline void setType(BlockType type) { m_type = type; }
-	
-	inline uint16_t getBlockCount() const { return m_pos.size(); }
-	inline void setBlockCount(uint16_t size) { m_pos.resize(size); }
-	
-	inline const Position& getPosition(uint16_t i) const { assert(i < m_pos.size()); return m_pos[i]; }
-	inline void setPosition(uint16_t i, const Position& pos) { assert(i < m_pos.size()); m_pos[i] = pos; }
-	
-	
-	uint16_t getHalfWidth() const;
-	uint16_t getBotHeight() const;
-	
-	void rotate(bool right = true);
-	
-	void setKind(BlockKind kind);
-	
-	void print() const;
-	void printPositions() const;
-	
-};
-*/
