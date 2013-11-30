2252146b1687ed72f7c4e80478b70a6e880f6a56
Eduardo San Martin Morote <posva13@gmail.com>
2013-11-30 10:41:16 +0100

    configure script

---
+++ configure.sh
@@ -0,0 +1,1 @@
+../configure-script/configure.sh
---
+++ do.sh
@@ -0,0 +1,12 @@
+
+#! /bin/bash
+
+# I usually have this kind of script in my project folder so that I don't have to manually write the configure options everytime I add new dependencies to the project
+# This one is what I use in my project InputManager:
+# https://github.com/posva/InputManager
+
+if [ `uname` = "Darwin" ]; then
+  ./configure.sh -aD -s src -o obj -b bin -c "xcrun clang++" -O "-Wall -Wextra -O2 -std=c++11 -stdlib=libc++" -Isrc -L/usr/local/lib -e cpp -E main -M Makefile -l "-framework sfml-system -framework sfml-window -framework sfml-graphics" -N "-std=c++11 -stdlib=libc++"
+else
+  ./configure.sh -aD -s src -o obj -b bin -c "gcc" -O "-Wall -Wextra -Os -std=c99 -D _BSD_SOURCE" -Isrc -L/usr/local/lib -e c -E tetris -M mk1 -l "-lncurses"
+fi
